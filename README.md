ce8ab3e42e2336000d184fbf54cf346f39c1c245
KeeganDavis <keegan.davis96@gmail.com>
2024-03-22 14:08:34 -0500

    finished all code minus bonus. still need to refactor and update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # belly-button-challenge
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach \
-- need to adjust size of plots \
+- need to adjust size of plots and add labels \
 - 
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,6 +13,7 @@ function init() {
       hoverinfo: 'text'
     }];
 
+    // default bubble chart data
     bubbleData = [{
         x: ['1', '2', '3', '4', '5'],
         y: [1, 2, 4, 8, 16],
@@ -24,56 +25,65 @@ function init() {
         text: ['a', 'b', 'c', 'd', 'e'],
     }];
   
+    // create default plots to load
     Plotly.newPlot("bar", barData);
     Plotly.newPlot("bubble", bubbleData)
   }
 
   function optionChanged() {
     dataPromise.then(data => {
-        const allNames = data['names'];
-        let dropdownMenu = d3.select('#selDataset');
-        let dataset = dropdownMenu.property('value');   
-        let nameIndex = allNames.indexOf(dataset);  
-        console.log(nameIndex)
+        /*
+        function to run everytime the dropdown menu is updated. the bar chart, bubble chart, and demographic info will update 
+        to display the samples for the selected individual
+        */
 
-        // collect all data for the samples and use that to get the sample values, otu ids, and otu labels
+        // get all name data
+        const allNames = data['names'];
+        // select the dropdown menu and store the value
+        let dropdownMenu = d3.select('#selDataset');
+        let dataset = dropdownMenu.property('value');
+        // find the index where the dropdow value is located in the names array   
+        let nameIndex = allNames.indexOf(dataset);  
+
+        // collect all data in arrays for the samples and use that to get the sample values, otu ids, and otu labels
         const allSamples = data['samples'];
         const sampleValues = allSamples.map(sample => sample['sample_values']);
         const otuIds = allSamples.map(sample => sample['otu_ids']);
         const otuLabels = allSamples.map(sample => sample['otu_labels']);
-        console.log(sampleValues[nameIndex].slice(0, 10).reverse())
 
+        // initialize arrays to hold our modified bar chart values
         let x = [];
         let y = [];
         let text = [];
 
+        // update the arrays to hold the top 10 OTUs found on the individual using the index
         x = sampleValues[nameIndex].slice(0, 10).reverse();
         y = otuIds[nameIndex].slice(0, 10).map(sample => 'OTU ' + sample.toString()).reverse();
         text = otuLabels[nameIndex].slice(0, 10).reverse();
 
+        // restyle the bar chart with the new individual's stats
         Plotly.restyle('bar', 'x', [x]);
         Plotly.restyle('bar', 'y', [y]);
         Plotly.restyle('bar', 'text', [text]);
 
+        // restyle the bubble chart with the new individual's stats
         Plotly.restyle('bubble', 'x', [otuIds[nameIndex]]);
         Plotly.restyle('bubble', 'y', [sampleValues[nameIndex]]);
         Plotly.restyle('bubble', 'text', [otuLabels[nameIndex]]);
         Plotly.restyle('bubble', 'marker.size', [sampleValues[nameIndex]]);
         Plotly.restyle('bubble', 'marker.color', [otuIds[nameIndex]]);
 
-        let demographicDiv = d3.select('#sample-metadata');
+        // store the array of all the metadata
         const metadata = data['metadata']
-        console.log(metadata[nameIndex]['ethnicity'])
-        console.log(metadata)
-        let metadataList = demographicDiv.append('ul')
 
-        metadataList.text(`id: ${metadata[nameIndex]['id']}`).attr('name', 'id')
-        metadataList.text(`ethnicity: ${metadata[nameIndex]['ethnicity']}`).attr('name', 'ethnicity')
-        metadataList.text(`gender: ${metadata[nameIndex]['gender']}`).attr('name', 'gender')
-        metadataList.text(`age: ${metadata[nameIndex]['age']}`).attr('name', 'age')
-        metadataList.text(`location: ${metadata[nameIndex]['location']}`).attr('name', 'location')
-        metadataList.text(`bbtype: ${metadata[nameIndex]['bbtype']}`).attr('name', 'bbtype')
-        metadataList.text(`wfreq: ${metadata[nameIndex]['wfreq']}`).attr('name', 'wfreq')
+        // update all previously created unordered list elements with the corresponding metadata for the selected individual
+        d3.select('[name="id"]').text(`id: ${metadata[nameIndex]['id']}`)
+        d3.select('[name="ethnicity"]').text(`ethnicity: ${metadata[nameIndex]['ethnicity']}`)
+        d3.select('[name="gender"]').text(`gender: ${metadata[nameIndex]['gender']}`)
+        d3.select('[name="age"]').text(`age: ${metadata[nameIndex]['age']}`)
+        d3.select('[name="location"]').text(`location: ${metadata[nameIndex]['location']}`)
+        d3.select('[name="bbtype"]').text(`bbtype: ${metadata[nameIndex]['bbtype']}`)
+        d3.select('[name="wfreq"]').text(`wfreq: ${metadata[nameIndex]['wfreq']}`)
 
     });    
   };
@@ -89,9 +99,11 @@ dataPromise.then(data => {
                 .attr('value', name)
     });
 
+    // select div for metadata and add unordered list
     let demographicDiv = d3.select('#sample-metadata');
     let metadataList = demographicDiv.append('ul')
 
+    // add list elements for each section of metadata with the corresponding name attribute
     metadataList.append('li').attr('name', 'id')
     metadataList.append('li').attr('name', 'ethnicity')
     metadataList.append('li').attr('name', 'gender')
